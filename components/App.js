bfa5bed029cb2e8755fe7a20831690e55de6af59
Alisa Yunusova <alisa.yunusova@gmail.com>
2017-05-25 10:56:22 +1200

    cloned and set up a dummy webpack only project

---
+++ components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+  }
+
+
+  render() {
+    return <div className="container">
+      <p>Enter Word:</p>
+      <input type="text" />
+    </div>
+  }
+
+}
+
+export default App
